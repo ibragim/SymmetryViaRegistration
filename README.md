96b7fc5807048fe325cb60a48906cf8776a51424
Marcelo Cicconet <cicconet@gmail.com>
2017-07-13 21:53:50 -0400

    ...

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SymmetryViaRegistration
 Mirror-Symmetry Detection via Registration
+
+Reference:
+Finding Mirror Symmetry via Registration
+Marcelo Cicconet, David G. C. Hildebrand, Hunter Elliott
+https://arxiv.org/abs/1611.05971
--- RunMe.m
+++ RunMe.m
@@ -1,10 +1,15 @@
 Image = imread('gantrycrane.png');
 Image = imrotate(Image,45,'crop');
 
-[angles, midPoints, segLengths, IOut] = symmetryViaRegistration2D(Image,'RegMethod','nxc');
+[angles, midPoints, segLengths, IOut] = symmetryViaRegistration2D(Image,'RegMethod','dense');
 
 subplot(1,2,1)
 imshow(Image), title('Input')
 
 subplot(1,2,2)
 imshow(IOut), title('Output')
+
+% Reference:
+% Finding Mirror Symmetry via Registration
+% Marcelo Cicconet, David G. C. Hildebrand, Hunter Elliott
+% https://arxiv.org/abs/1611.05971
